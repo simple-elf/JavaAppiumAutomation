0f9aadeab68296c8607ceb0c49d66951f9176612
Aaftakhov <simple1elf@gmail.com>
2018-05-30 10:12:16 +0500

    Lesson #2.1: First element search and click

--- src/FirstTest.java
+++ src/FirstTest.java
@@ -3,6 +3,7 @@ import io.appium.java_client.android.AndroidDriver;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
 import java.net.URL;
@@ -34,7 +35,9 @@ public class FirstTest {
 
     @Test
     public void firstTest() {
-        System.out.println("First test run");
+        WebElement search = driver.findElementByXPath("//*[contains(@text, 'Search Wikipedia')]");
+        search.click();
+        //System.out.println("First test run");
     }
 
 }
