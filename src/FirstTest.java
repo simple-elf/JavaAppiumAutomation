e3fe429e419f5190a288893806b645801f719504
Aaftakhov <simple1elf@gmail.com>
2018-05-30 10:24:15 +0500

    Lesson #2.3: Wait

--- src/FirstTest.java
+++ src/FirstTest.java
@@ -3,8 +3,12 @@ import io.appium.java_client.android.AndroidDriver;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.net.URL;
 
@@ -37,9 +41,16 @@ public class FirstTest {
     public void firstTest() {
         WebElement search_init = driver.findElementByXPath("//*[contains(@text, 'Search Wikipedia')]");
         search_init.click();
-        WebElement search_input = driver.findElementByXPath("//*[contains(@text, 'Search…')]");
+
+        WebElement search_input =  waitForElementPresentByXpath("//*[contains(@text, 'Search…')]", "Cannot find search input", 5);
         search_input.sendKeys("Appium");
-        //System.out.println("First test run");
+    }
+
+    private WebElement waitForElementPresentByXpath(String xpath, String errorText, long timeoutInSeconds) {
+        WebDriverWait wait = new WebDriverWait(driver, timeoutInSeconds);
+        wait.withMessage(errorText + "\n");
+        By by = By.xpath(xpath);
+        return wait.until(ExpectedConditions.presenceOfElementLocated(by));
     }
 
 }
