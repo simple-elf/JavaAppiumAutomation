4cf93280ec45c08d51ab353e615e651d23129cb6
Aaftakhov <simple1elf@gmail.com>
2018-06-18 14:25:16 +0500

    Lesson #5.11: Ignore Welcome test, because we skip welcome screen on starting driver

--- src/tests/GetStartedTest.java
+++ src/tests/GetStartedTest.java
@@ -5,10 +5,12 @@ import lib.Platform;
 import lib.ui.WelcomePageObject;
 import org.junit.Assert;
 import org.junit.Assume;
+import org.junit.Ignore;
 import org.junit.Test;
 
 public class GetStartedTest extends CoreTestCase {
 
+    @Ignore("Добавлен пропуск обучения при старте драйвера")
     @Test
     public void testPassThroughWelcome() {
         Assume.assumeTrue("This test is ignored on Android", Platform.getInstance().isIOS());
